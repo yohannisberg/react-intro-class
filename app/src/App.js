2086d8b3b6257ac3a274b7044be733c3288be7c8
John D'Agostino <jdagos3@gmail.com>
2017-05-16 15:59:41 -0600

    finished project minus the black diamond

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Calculator from './Components/Calculator/Calculator';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
